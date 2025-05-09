95f8a006235738cfbc41a72970a9a8e06e67980d
dhruv716 <121818483+dhruv716@users.noreply.github.com>
2025-05-09 16:49:55 +0530

    Update Interface.java

--- 8/Interface.java
+++ 8/Interface.java
@@ -1,8 +1,10 @@
-import java.rmi.*;
-import java.util.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Map;
 
 public interface HotelServiceInterface extends Remote {
     boolean bookRoom(String guestName, int roomNumber) throws RemoteException;
     boolean cancelBooking(String guestName) throws RemoteException;
     Map<Integer, String> showBookedRoomDetails() throws RemoteException;
 }
+
